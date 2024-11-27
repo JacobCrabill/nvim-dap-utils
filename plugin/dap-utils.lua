a169a114354e6da584d34d878887cb7980b357f5
Jacob Crabill <jcrabill@darkhive.com>
2024-11-27 10:15:08 -0800

    Fix typos in user commands

--- plugin/dap-utils.lua
+++ plugin/dap-utils.lua
@@ -1,7 +1,7 @@
 -- Expose relevant functions as user commands
-vim.api.nvim_create_user_command('DebugCMake', function() require('dap_utils').cmake_binary_picker() end, {})
-vim.api.nvim_create_user_command('DebugZig', function() require('dap_utils').zig_picker() end, {})
-vim.api.nvim_create_user_command("LoadDAPConfig", function() require('dap_utils').load_local_config() end, {})
-vim.api.nvim_create_user_command("ShowDAPConfigs", function() require('dap_utils').show_dap_configs() end, {})
-vim.api.nvim_create_user_command("ShowDAPAdapters", function() require('dap_utils').show_dap_adapters() end, {})
-vim.api.nvim_create_user_command("CreateLocalDAPConfig", function() require('dap_utils').create_starter_local_config() end, {})
+vim.api.nvim_create_user_command('DebugCMake', function() require('dap-utils').cmake_binary_picker() end, {})
+vim.api.nvim_create_user_command('DebugZig', function() require('dap-utils').zig_picker() end, {})
+vim.api.nvim_create_user_command("LoadDAPConfig", function() require('dap-utils').load_local_config() end, {})
+vim.api.nvim_create_user_command("ShowDAPConfigs", function() require('dap-utils').show_dap_configs() end, {})
+vim.api.nvim_create_user_command("ShowDAPAdapters", function() require('dap-utils').show_dap_adapters() end, {})
+vim.api.nvim_create_user_command("CreateLocalDAPConfig", function() require('dap-utils').create_starter_local_config() end, {})
